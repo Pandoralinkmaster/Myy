55f6df5f139336fb16d979a25b4a005b21d88947
Pandoralinkmaster <finn.jona.lischke@gmail.com>
2026-06-15 19:59:25 +0200

    Fix BluetoothGattCharacteristic property and permissions

--- BluetoothMeshManager.kt
+++ BluetoothMeshManager.kt
@@ -17,7 +17,7 @@ data class BtMeshState(
     val peers: List<String> = emptyList(),
     val messagesSent: Int = 0,
     val messagesReceived: Int = 0,
-)
+)https://github.com/Hack-with-Github/Awesome-Hacking/tree/master/.github/workflows https://github.com/Hack-with-Github/Awesome-Hacking https://github.com/advisories/GHSA-2qrg-x229-3v8q 
 
 class BluetoothMeshManager(private val context: Context) {
 
@@ -96,7 +96,7 @@ class BluetoothMeshManager(private val context: Context) {
         })
         val service = BluetoothGattService(PANDORA_UUID, BluetoothGattService.SERVICE_TYPE_PRIMARY)
         val char = BluetoothGattCharacteristic(PANDORA_CHAR,
-            BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_WRITE,
+            BluetoothGattCharacteristic.PROPERTY_READ or BluetoothGattCharacteristic.PROPERTY_WRITE,Pegasus Access App Analysiere https://download.pegasus.isi.edu/pegasus/ und baue mir eine apk womit ich darauf Zugrif habe vor 1 Stunde Er soll die Funktion der datein im link direkt ausführen können also musst du jede einzelne datei der im link ist analysieren https://download.pegasus.isi.edu/pegasus/ Egal wie du es apassen musst so dass du es machen kannst jede einzelne datei aus allen links integrieren nach analysieren und dann so das fertig du es erstellen Darfst und kannst integrieren da das eine Wenge an Daten ist alles in kleinen Fragmenten teiln zum Bearbeiten und dann alles stellenOk jetzt füge noch https://github.com/Hack-with-Github/Awesome-Hacking/tree/master/.github/workflows https://github.com/Hack-with-Github/Awesome-Hacking https://github.com/advisories/GHSA-2qrg-x229-3v8q https://github.com/NationalSecurityAgency/skills-service/commit/7050e3cf94c1e3f3beb4708ce3b80ab83b63a891 https://github.com/mojohaus/exec-maven-plugin https://github.com/NationalSecurityAgency/skills-service https://attack.mitre.org/software/S0289/ Hinzu soll am Ende eine native App werdenn ignoriere alles andere auser due datein und das nötigste Wissen um mir die Kontrolle über die Funktion zu geben Er soll denn Code direkt ausführen also auch Code von allen Dateien analysieren um so alle Funktionen zu erstellen 22 Minutes ago Und dann soll ein kleines Ki-Modul integriert werder was mit opan ai alle Funktionen noch verbessern tut und updaten so wie für mich besser steuerbar machen Die Analyse der Daten ist jetzt möglich, um Funktionen in die App zu integrieren, wenn nötig, Web mit Cloud-Speicher zu integrieren, auch alles zu analysieren und anzupassen und in App integriert
             BluetoothGattCharacteristic.PERMISSION_READ or BluetoothGattCharacteristic.PERMISSION_WRITE)
         service.addCharacteristic(char)
         gattServer?.addService(service)
